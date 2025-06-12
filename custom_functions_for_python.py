39c7cb2b783cd933bad7da8cac868d0fda44a346
Joseph Ribeiro (Cefas) <joseph.ribeiro@cefas.gov.uk>
2025-06-12 15:30:35 +0100

    Change to stop overwriting the old model object (revisiting this)

--- custom_functions_for_python.py
+++ custom_functions_for_python.py
@@ -341,7 +341,7 @@ def calibrateClassifier(fitted_final_classifier, validation_set, target_name, gr
   print("Done ! Saving calibrated classifier ...")
   
   timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-  filename_finalCalibratedModel = os.path.join(model_dir, f"final_model_{timestamp}.pkl")
+  filename_finalCalibratedModel = os.path.join(filename_finalCalibratedModel, f"final_model_{timestamp}.pkl")
   
   joblib.dump(fitted_calibrated_classifier, filename_finalCalibratedModel)
   
